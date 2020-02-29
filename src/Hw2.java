7dd6a07dc9bb91631116fe6195cbef134aa9a050
OulaMardawi <ola.mardawi@gmail.com>
2020-02-29 17:26:26 +0200

    My second Trail

---
+++ src/Hw2.java
@@ -0,0 +1,24 @@
+
+public class Hw2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static int add (int a , int b) {
+		return a+b;
+	}
+	
+	public static int sub (int a , int b) {
+		return a-b;
+	}
+	
+	public static int mul (int a , int b) {
+		return a*b;
+	}
+	
+	public static int div (int a , int b) {
+		return a/b;
+	}
+
+}
